ea6b99d1b7355d960d1d374de4ce296cc6c43c17
Michael Redig <mredig@gmail.com>
2019-07-29 21:22:52 -0500

    Initial Commit

---
+++ Theoretical collection Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Theoretical collection Table
+//
+//  Created by Michael Redig on 7/29/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
